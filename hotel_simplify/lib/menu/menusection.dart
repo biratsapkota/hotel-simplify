b0971a312bb87d2b12a657d41c614e46b20fba33
Birat Sapkota <biratsapkota9@gmail.com>
2019-07-09 17:14:05 +0545

    some changes

--- hotel_simplify/lib/menu/menusection.dart
+++ hotel_simplify/lib/menu/menusection.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_container/responsive_container.dart';
 
@@ -19,12 +20,13 @@ class _MenuSectionState extends State<MenuSection> {
             child: Container(
               padding: const EdgeInsets.all(30),
               color: Colors.purple[900],
-              child: Column(
+              child: ListView(
                 children: <Widget>[
                   Row(
                     children: <Widget>[
                       Flexible(
                         child: TextField(
+                          style: TextStyle(color: Colors.white),
                           decoration: InputDecoration(
                               border: OutlineInputBorder(),
                               suffixIcon: Icon(Icons.search)),
@@ -61,10 +63,7 @@ class _MenuSectionState extends State<MenuSection> {
               ),
             ),
           ),
-          ResponsiveContainer(
-            heightPercent: 100,
-            widthPercent: 50,
-          ),
+          orderList,
         ],
       ),
     );
@@ -104,52 +103,132 @@ Widget menuHeader = Row(
 );
 
 Widget menu = ResponsiveContainer(
-  heightPercent: 60,
+  heightPercent: 70,
   widthPercent: 100,
   child: ListView(
     children: <Widget>[
       Wrap(
+        spacing: 20,
+        runSpacing: 20,
         children: <Widget>[
-          menuItem
+          menuItem('https://placeimg.com/600/600/nature', 'Beer'),
+          menuItem('https://placeimg.com/600/600/nature', 'Chowmein'),
+          menuItem('https://placeimg.com/600/600/nature', 'Waffles'),
+          menuItem('https://placeimg.com/600/600/nature', 'Coffee'),
+          menuItem('https://placeimg.com/600/600/nature', 'Dessert'),
+          menuItem('https://placeimg.com/600/600/nature', 'Pizza'),
         ],
       ),
     ],
   ),
 );
 
-Widget menuItem = Stack(
-  children: <Widget>[
-    Container(
-      width: 100,
-      child: Image.asset('assets/beer.jpg'),
-      decoration: new BoxDecoration(boxShadow: [
-        new BoxShadow(
-          color: Colors.black87,
-          blurRadius: 0,
-          // has the effect of extending the shadow
-          offset: Offset(
-            4.0, // horizontal, move right 10
-            5.0, // vertical, move down 10
-          ),
+Widget menuItem(String imgUrl, String itemName) => Stack(
+      children: <Widget>[
+        Container(
+          width: 100,
+          child: Image.network(imgUrl),
+          decoration: new BoxDecoration(boxShadow: [
+            new BoxShadow(
+              color: Colors.black,
+              blurRadius: 3,
+              // has the effect of extending the shadow
+              offset: Offset(
+                4.0, // horizontal, move right 10
+                5.0, // vertical, move down 10
+              ),
+            ),
+          ]),
         ),
-      ]),
-    ),
-    Opacity(
-      opacity: 0.5,
-      child: Container(
-        color: Colors.black,
-        height: 50,
-        width: 100,
-        child: Align(
-          alignment: Alignment
-              .center, // Align however you like (i.e .centerRight, centerLeft)
-          child: Text(
-            'Beer',
-            style: TextStyle(color: Colors.white),
+        Opacity(
+          opacity: 0.5,
+          child: Container(
+            color: Colors.black,
+            height: 50,
+            width: 100,
+            child: Align(
+              alignment: Alignment
+                  .center, // Align however you like (i.e .centerRight, centerLeft)
+              child: Text(
+                itemName,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
           ),
+        )
+      ],
+      alignment: AlignmentDirectional.bottomCenter,
+    );
+
+Widget orderList = ResponsiveContainer(
+  heightPercent: 100,
+  widthPercent: 50,
+  child: Column(
+    children: <Widget>[
+      Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Text(
+              'Orderlist',
+              textAlign: TextAlign.end,
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              children: <Widget>[
+                Radio(
+                  value: true,
+                  onChanged: (value) {},
+                  groupValue: 1,
+                ),
+                Text('Merge Tables  '),
+                Text('   |   '),
+                Text('Bill No.  '),
+                Icon(Icons.add_circle,color:Colors.grey)
+              ],
+            ),
+            Table(
+              textDirection: TextDirection.ltr,
+              children: [
+                TableRow(
+                    decoration: BoxDecoration(
+                        border: Border(
+                            bottom: BorderSide(width: 1, color: Colors.black))),
+                    children: [
+                      Text('S.N.',style: TextStyle( fontWeight: FontWeight.bold)),
+                      Text('Items',style: TextStyle( fontWeight: FontWeight.bold)),
+                      Text('Quantity',style: TextStyle( fontWeight: FontWeight.bold)),
+                      Text('Extra',style: TextStyle( fontWeight: FontWeight.bold))
+                    ]),
+                tableRow,
+                tableRow,
+                tableRow,
+                tableRow
+              ],
+            ),
+            //FlatButton(onPressed: (){},child: Text('Send Order'),)
+          ],
         ),
-      ),
-    )
-  ],
-  alignment: AlignmentDirectional.bottomCenter,
+      )
+    ],
+  ),
 );
+
+TableRow tableRow = TableRow(
+    decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(width: 1, color: Colors.grey))),
+    children: [
+      Text('01'),
+      Text('Chopsuey'),
+      Row(
+        children: <Widget>[
+          Icon(Icons.remove_circle,color:Colors.grey),
+          SizedBox(width: 5.0),
+          Text('1'),
+          SizedBox(width: 5.0),
+          Icon(Icons.add_circle,color:Colors.grey)
+        ],
+      ),
+      Icon(Icons.restaurant_menu)
+    ]);
